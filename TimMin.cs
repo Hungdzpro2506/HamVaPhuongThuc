286343fa04f58795130c22daf06d5d40e73b0d54
Hungdzpro2506 <134621730+Hungdzpro2506@users.noreply.github.com>
2024-11-22 15:00:40 +0700

    Add files via upload

---
+++ TimMin.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class TimMin
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 5, 1, 4, 0, 4, 1, 3, 5 };
+            int index = TimMinnn(arr);
+            Console.WriteLine("Min cua mang la "+ arr[index]);
+        }
+        public static int TimMinnn(int []arr)
+        {
+            int min = arr[0];
+            int index = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min= arr[i];
+                    index = i;
+                }
+            }
+            return index;
+
+        }
+    }
+}
